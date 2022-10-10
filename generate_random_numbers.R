4a8f330e75ea6bf618e30972e234ca022aa615f8
“Student492” <sandip.rijal55@gmail.com>
2022-10-10 08:15:30 -0400

    add git ignore file

--- generate_random_numbers.R
+++ generate_random_numbers.R
@@ -1,3 +1,10 @@
-a=rnorm(1000, 0.5, 0.0833)
+#generate random number
+a=rnorm(1000, 25, 8)
+#check for the values in a
 a
+#mean of a
 mean(a)
+#sum of all values in a
+sum(a)
+#standard deviation of all the values in a 
+sd(a)
